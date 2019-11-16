b7cd0310ee6d412a3f22ae8ad20621ae38dae44d
Rishmawi Jean-Tarek <jean-tarek@yusofleet.com>
2019-11-16 19:30:49 +0100

    card demo

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const Card = styled.div`
   display: flex;
   justify-content: center;
-  align-items: center;
+  align-items: flex-end;
   width: 100%;
   height: 100%;
   background-image: url(${props => props.bgImg});
