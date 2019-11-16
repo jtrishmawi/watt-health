1282c54e532f160b7959a1293051a23e16ddb68d
Rishmawi Jean-Tarek <jean-tarek@yusofleet.com>
2019-11-16 10:44:31 +0100

    Updates

---
+++ precache-manifest.4d398a74d8ca0a8cce5b9ffb69eb1cc3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5d80d209dbc6999cc161ceaf4d64896",
+    "url": "/watt-health/index.html"
+  },
+  {
+    "revision": "7237d40709128c45916d",
+    "url": "/watt-health/static/css/main.da0491d2.chunk.css"
+  },
+  {
+    "revision": "81e46cf85f74dd846b07",
+    "url": "/watt-health/static/js/2.5fefb58c.chunk.js"
+  },
+  {
+    "revision": "7237d40709128c45916d",
+    "url": "/watt-health/static/js/main.e27dd2b1.chunk.js"
+  },
+  {
+    "revision": "065e2e4a647aaaac6d50",
+    "url": "/watt-health/static/js/runtime-main.5c6da236.js"
+  },
+  {
+    "revision": "fc22f6d45c1e2529d473c8da4073acf6",
+    "url": "/watt-health/static/media/logo.fc22f6d4.svg"
+  }
+]);
